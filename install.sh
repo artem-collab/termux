d8fe55d3ca2850a9fbb9679454146790993389d5
artem-collab <58644376+artem-collab@users.noreply.github.com>
2020-01-25 22:27:52 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pkg install python
-pip install -r /data/data/com.termux/files/home/termux
+pip install -r /data/data/com.termux/files/home/termux/setup.txt
 python /data/data/com.termux/files/home/termux/menu.py
 termux-setup-storage
